f09d4b0e4d2c7c9f9fbaf75b3fe47377ae790794
Greg Smith <smr.kyu@gmail.com>
2014-09-25 01:48:48 -0500

    Support for stdin/out and file in/out in CLI

--- cli.js
+++ cli.js
@@ -1,22 +1,65 @@
 #!/usr/bin/env node
 
+var concat = require('concat-stream');
+var fs = require('fs');
 var program = require('commander');
+
 var pkg = require('./package.json');
 var opt = require('./index');
 
 program
 	.version(pkg.version)
-	.option('-i, --in [format]', 'Define the input MML format.', 'aa')
-	.option('-o, --out [format]', 'Define the output MML format.', 'aa');
+	.option('-i, --infile [file]', 'set the filename for input (default: stdin)')
+	.option('-o, --outfile [file]', 'set the filename for output (default: stdout)')
+	.option('-f, --format [format]', 'set the input and output MML format')
+	.option('-I, --infmt [format]', 'set the input MML format')
+	.option('-O, --outfmt [format]', 'set the output MML format');
 
 program.on('--help', function () {
 	console.log('  Supported formats:');
 	console.log();
-	console.log('    aa                  ArcheAge (default)');
-	console.log('    mabi                Mabinogi');
+	console.log('    aa         ArcheAge (default)');
+	console.log('    mabi       Mabinogi');
 });
 
 program.parse(process.argv);
 
-var optimized = opt(program.args.join(), { input: program.in, output: program.out });
-process.stdout.write(optimized);
+if (program.format && program.infmt) {
+	console.error('Error: cannot use both --format and --infmt');
+	process.exit(1);
+}
+if (program.format && program.outfmt) {
+	console.error('Error: cannot use both --format and --outfmt');
+	process.exit(1);
+}
+
+var options = {
+	input: program.format || program.infmt || 'aa',
+	output: program.format || program.outfmt || 'aa'
+};
+
+if (program.infile) {
+	fs.readFile(program.infile, function (err, data) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		run(data);
+	});
+} else {
+	process.stdin.pipe(concat(run));
+}
+
+function run(mml) {
+	var optimized = opt(mml, options);
+	if (program.outfile) {
+		fs.writeFile(program.outfile, optimized, function (err) {
+			if (err) {
+				console.error(err);
+				process.exit(1);
+			}
+		});
+	} else {
+		process.stdout.write(optimized);
+	}
+}
