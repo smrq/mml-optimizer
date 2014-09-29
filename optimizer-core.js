20d1c4d9b9fa0cbb50524edd404d1ff41b98f6fc
Greg Smith <smr.kyu@gmail.com>
2014-09-29 00:12:40 -0500

    Removed option aliases from optimizer-core

--- optimizer-core.js
+++ optimizer-core.js
@@ -1,38 +1,6 @@
 var aStar = require('a-star');
 var extend = require('extend');
 
-var optionAliases = {
-	'mabi': {
-		tpqn: 96,
-		minimumNoteDuration: 64,
-		defaultState: {
-			octave: 4,
-			tempo: 120,
-			volume: 8/15,
-			duration: '4'
-		},
-		maxVolume: 15,
-		supportsNoteNumbers: true,
-		tracksShareState: false,
-		octaveOffset: 0
-	},
-	'aa': {
-		tpqn: 500,
-		minimumNoteDuration: 64,
-		defaultState: {
-			octave: 4,
-			tempo: 120,
-			volume: 100/127,
-			duration: '4'
-		},
-		maxVolume: 127,
-		supportsNoteNumbers: false,
-		tracksShareState: true,
-		octaveOffset: -1
-	}
-}
-var defaultOptions = optionAliases['aa'];
-
 function noteDurationToTicks(duration, options) {
 	return noteDurationAndDotsToTicks(
 		parseInt(duration, 10),
